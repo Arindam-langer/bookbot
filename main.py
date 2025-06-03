36993c8669bb1233897ea362f42851c4dc5f2729
Arindam-langer <arindamhateswork@gmail.com>
2025-06-03 19:14:42 +0530

    add a file called stats.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greeting boots!!")
+import stats as st
+
+BOOKPATH = "books/frankenstein.txt"
+#reads the content of the book
+def get_book_text(filepath:str) ->str:
+    with open(filepath) as f:
+        f_content = f.read()
+    return f_content ##return string
+      
+
+def main():
+    book_text = get_book_text(BOOKPATH)
+    num_words = st.num_of_words(book_text)
+    print(f"{num_words} words found in the document")
+    print(st.count_char(book_text))
+if __name__ == "__main__":
+    main() 
